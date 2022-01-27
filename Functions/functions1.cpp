e99cb2390575b9d762ce47f2a7a8dd708283f778
Arpit Chaurasia <benchaurasia@gmail.com>
2022-01-28 01:56:19 +0530

    added todecimal function

--- Functions/functions1.cpp
+++ Functions/functions1.cpp
@@ -23,10 +23,23 @@ bool pytho(int x,int y,int z){
 	return false;
 }
 
+//binary, octal, hexadecimal to decimal
+int todecimal(string s,int n){
+	char a[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+	int l = s.size()-1, sum=0;
+	for(auto i : s){
+		auto itr = find(a,a+16,i);
+		cout<<(itr-a)<<' ';
+		sum += (itr-a)*pow(n,l);
+		l--;
+	}
+	cout<<endl;
+	return sum;
+}
+
 int main(){
 	int a,b,c;
 	cin>>a>>b>>c;
-	if(pytho(a,b,c)) cout<<"pytho";
-	else cout<<"not pytho";
+	cout<<todecimal(to_string(a),b);
 	return 0;
 }
