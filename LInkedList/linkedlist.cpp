d60ce585d478ace6155dba6af2d580a935cfbb3d
Arpit Chaurasia <benchaurasia@gmail.com>
2022-03-16 17:33:49 +0530

    added merge functions

--- LInkedList/linkedlist.cpp
+++ LInkedList/linkedlist.cpp
@@ -284,6 +284,57 @@ void intersectionPoint(Node* &head1, Node* &head2){
 	cout<<"No Intersection!!\n";
 }
 
+//merge two sorted linkedlist (itearative way)
+Node* mergelists(Node* &head1, Node* &head2){
+	Node* p1 = head1, *p2 = head2, *dummyNode = new Node(-1);
+	Node* p3 = dummyNode;
+
+	while(p1!=NULL && p2!=NULL){
+		if(p1->data < p2->data){
+			p3->next = p1;
+			p1 = p1->next;
+		}
+		else {
+			p3 ->next = p2;
+			p2 = p2->next;
+		}
+		p3 = p3->next;
+	}
+	while(p1!=NULL){
+		p3->next = p1;
+		p1 = p1->next;
+		p3 = p3 ->next;
+	}
+	while(p2!=NULL){
+		p3->next = p2;
+		p2 = p2->next;
+		p3 = p3->next;
+	}
+	return dummyNode->next;
+}
+
+//merge two linked lists (recursive way)
+Node* mergelistsRecursive(Node* &head1, Node* head2){
+
+	if(head1 == NULL){
+		return head2;
+	}
+	if(head2 == NULL){
+		return head1;
+	}
+	Node* result;
+	if(head1->data < head2->data){
+		result = head1;
+		result->next = mergelistsRecursive(head1->next,head2);
+	}
+	else{
+		result = head2;
+		result->next = mergelistsRecursive(head1,head2->next);
+	}
+	return result;
+}
+
+
 void solve(){
 	LL n,x;
 	cin>>n;
@@ -323,11 +374,14 @@ void solve(){
 		cin>>x;
 		insertAtTail(head2,x);
 	}
-	intersect(head,head2,key);
-	intersectionPoint(head,head2);
+	// intersect(head,head2,key);
+	// intersectionPoint(head,head2);
 
 	display(head);
 	display(head2);
+
+	Node* newhead = mergelistsRecursive(head,head2);
+	display(newhead);
 }
 
 
