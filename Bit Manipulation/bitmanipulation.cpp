a9006340344415a96729f93247a0a5bae18b5963
Arpit Chaurasia <benchaurasia@gmail.com>
2022-08-14 02:14:01 +0530

    added : useful-builtin-functions

--- Bit Manipulation/bitmanipulation.cpp
+++ Bit Manipulation/bitmanipulation.cpp
@@ -1,15 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long LL;
+typedef long long ll;
 typedef pair<int, int> pii;
-typedef pair<LL, LL> pll;
+typedef pair<ll, ll> pll;
 typedef pair<string, string> pss;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<pii> vii;
-typedef vector<LL> vl;
+typedef vector<ll> vl;
 typedef vector<vl> vvl;
+typedef unsigned long long ull;
+typedef long double lld;
 
 #define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a))
 #define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a))
@@ -19,51 +21,82 @@ typedef vector<vl> vvl;
 #define FOREACH(a, b) for (auto&(a) : (b))
 #define REP(i, n) FOR(i, 0, n)
 #define REPN(i, n) FORN(i, 1, n)
-#define MAX(a, b) a = max(a, b)
-#define MIN(a, b) a = min(a, b)
-#define SQR(x) ((LL)(x) * (x))
+#define SQR(x) ((ll)(x) * (x))
 #define RESET(a, b) memset(a, b, sizeof(a))
 #define fi first
 #define se second
 #define mp make_pair
 #define pb push_back
+#define ppb pop_back
 #define ALL(v) v.begin(), v.end()
+#define RALL(v) v.rbegin(), v.rend()
 #define ALLA(arr, sz) arr, arr + sz
-#define SIZE(v) (int)v.size()
+#define sz(v) (int)v.size()
 #define SORT(v) sort(ALL(v))
+#define RSORT(v) sort(RALL(v))
 #define REVERSE(v) reverse(ALL(v))
 #define SORTA(arr, sz) sort(ALLA(arr, sz))
 #define REVERSEA(arr, sz) reverse(ALLA(arr, sz))
 #define PERMUTE next_permutation
 #define TC(t) while (t--)
+#define MOD 1e9+7
+#define INF 1e18
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.fi); cerr << ","; _print(p.se); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
 
 // BIT MANIPULATIONS - BASIC OPERATIONS
 //
 //
 // getBit() - gives the bit value at given position
-LL getBit(LL n, LL pos){
+ll getBit(ll n, ll pos){
 	return ((n & (1 << pos)) !=0);
 }
 
 // setBit() - sets the bit value to '1' at the given position
-LL setBit(LL n, LL pos){
+ll setBit(ll n, ll pos){
 	return (n | (1 << pos));
 }
 
 // clearBit() - clears the bit value to '0' at the given position
-LL clearBit(LL n, LL pos){
+ll clearBit(ll n, ll pos){
 	return (n & ~(1 << pos));
 }
 
 // updateBit() - updates the bit at the given position first by clearing it, then updating it to given value.
-LL updateBit(LL n, LL pos, LL bit){
+ll updateBit(ll n, ll pos, ll bit){
 	clearBit(n,pos);
 	return (n | (bit << pos));
 }
 
 //check if a given number is power of 2
 //returns 1 if n is power of 2 else 0
-bool chckPowerofTwo(LL n){
+bool chckPowerofTwo(ll n){
 	return (n && !(n & n-1));
 }
 
@@ -71,8 +104,8 @@ bool chckPowerofTwo(LL n){
 // count and return the number of ones in binary form for a given number
 //
 // also __builtin_popcount(x) does the same
-LL countNumberOfOnes(LL n){
-	LL count = 0;
+ll countNumberOfOnes(ll n){
+	ll count = 0;
 	while(n){
 		n = n & n-1;
 		count++;
@@ -83,7 +116,7 @@ LL countNumberOfOnes(LL n){
 
 //generate all possible subsets of a set
 void generateSubsets(vi v){
-	LL l = v.size();
+	ll l = v.size();
 	REP(i , (1<<l)){
 		REP(j,l){
 			if( i & (1 << j)) { // checking for setbit
@@ -127,7 +160,7 @@ void findTwoUniqueNumber(vi v){
 
 //find unique number in a vector where all other numbers are present thrice
 void tripletsExceptOne(vi v){
-	LL result = 0;
+	ll result = 0;
 	REP(i,64){
 		int sum = 0;
 		FOREACH(j,v){
@@ -140,14 +173,44 @@ void tripletsExceptOne(vi v){
 	cout<<result;
 }
 
+//-------------------------------------------------- BUILTIN_FUNCTIONS -------------------------------------------------------
+
+void builtin(string s){ // taking binary string as input
+	bitset<16> bt(s);	// changing to bitset
+	ll b = bt.to_ullong(); // converting to long long value
+	
+	// 1) __builtin_popcount(): Used to give the no. of ones.
+	cout << __builtin_popcount(b)<<endl;
+	
+	// 2) __builtin_parity(): Used to give the parity. If '1' therefore odd count of ones and '0' means even count.
+	cout << __builtin_parity(b) << endl;
+
+	// 3) __builtin_clz(): Used to give leading number of zeroes till the MSB (most significant bit).
+	cout << __builtin_clz(b) << endl;
+
+	// 4) __builtin_ctz(): Used to give the trailing number of zeroes after the LSB(least significant bit).
+	cout << __builtin_ctz(b) << endl;
+
+	// 5) __builtin_ffs(): Used to give the position of the LSB + 1.
+	cout << __builtin_ffs(b) << endl;
+
+	// 6) __lg():
+	cout << __lg(b) << endl;
+
+	// 7) __builtin_bswap16(): Used to reverse the order of bits. Can also use bswap32, bswap64 and bswap128 as per requirement.
+	cout << __builtin_bswap16(b) << endl;
+}
+
+
+//------------------------------------------------- FUNCTION CALLS ------------------------------------------------------------------
 
 void solve(){
-	int n;
-	cin>>n;
-	int k;
-	cin>>k;
+	// int n;
+	// cin>>n;
+	// int k;
+	// cin>>k;
 
-	cout<<getBit(n,k);
+	// cout<<getBit(n,k);
 	// cout<<setBit(n,k);
 	// cout<<clearBit(n,k);
 	// cout<<updateBit(n,k,1);
@@ -166,12 +229,20 @@ void solve(){
 	// findTwoUniqueNumber(v);
 
 	// tripletsExceptOne(v);
+
+	string s;
+	cin>>s;
+	builtin(s);
 }
 
 
 int main(){
-	ios_base::sync_with_stdio(0);
-    cin.tie(0);
+	#ifndef ONLINE_JUDGE
+	freopen("Error.in","w",stderr);
+	#endif
+	ios_base::sync_with_stdio(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
     //int t;
     //cin>>t;
     //TC(t)
