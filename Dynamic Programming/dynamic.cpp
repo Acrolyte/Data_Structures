8faa11c113a74c6bf0cb9d74792384259a176ada
Arpit Chaurasia <benchaurasia@gmail.com>
2022-09-17 04:12:42 +0530

    added problems on 0-1 Knapsack

--- Dynamic Programming/dynamic.cpp
+++ Dynamic Programming/dynamic.cpp
@@ -96,7 +96,7 @@ ll zeroOneKnapsackMemoized(ll wt[],ll vl[],ll W, ll n,vvl &dp){
 	return dp[n][W];
 }
 //
-// DP WAY ()
+// DP WAY (Top Down)
 ll zeroOneKnapsackTopDown(ll wt[],ll vl[],ll W, ll n){
 	vvl dp(n+1,vector<ll> (W+1));
 
@@ -109,8 +109,133 @@ ll zeroOneKnapsackTopDown(ll wt[],ll vl[],ll W, ll n){
 			if(wt[i-1]<=j)
 				dp[i][j] = max(vl[i-1]+dp[i-1][j-wt[i-1]], dp[i-1][j]);
 			else dp[i][j] = dp[i-1][j];
-
+	// debug(dp)
 	return dp[n][W];
+}
+//
+// Problems related to 0-1 Knapsack :-
+//
+// 1. Subset Sum: Check if there exists a sum of numbers equal to the given weight W.
+// 
+bool subsetSum(ll val[],ll W, ll n){
+	vector<vector<bool>> dp(n+1, vector<bool> (W+1));
+
+	REP(i,n+1) REP(j,W+1) {
+		if(i==0) dp[i][j] = false;
+		if(j==0) dp[i][j] = true;
+	}
+	for(ll i=1;i<n+1;i++)
+		for(ll j=1;j<W+1;j++)
+			if(val[i-1] <= j)
+				dp[i][j] = (dp[i-1][j-val[i-1]] or dp[i-1][j]);
+			else dp[i][j] = dp[i-1][j];
+
+	// for(vector<bool> i: dp){ auto it = i; debug(it);}
+	return dp[n][W]; 
+}
+//
+// 2. Equal Sum Partition Problem: Check if the given numbers can be divided into two parts of equal values.
+//
+bool equalSumPartition(ll val[], ll n){
+	ll sum = 0;
+	REP(i,n) sum += val[i];
+
+	if(sum & 1) return false;
+	else {
+		sum >>= 1;
+
+		// vector<vector<bool>> dp(n+1, vector<bool> (sum+1));
+
+		// REP(i,n+1) REP(j,sum+1){
+		// 	if(i==0) dp[i][j] = false;
+		// 	if(j==0) dp[i][j] = true;
+		// }
+
+		// for(ll i=1;i<n+1;i++)
+		// for(ll j=1;j<sum+1;j++){
+		// 	if(val[i-1] <= j)
+		// 		dp[i][j] = (dp[i-1][j-val[i-1]] or dp[i-1][j]);
+		// 	else dp[i][j] = dp[i-1][j];
+		// }
+
+		// for(vector<bool> i: dp){ auto it = i; debug(it);}
+		// return dp[n][sum];
+
+
+		// Typed everything but lets call subsetSum(). :P
+
+		return subsetSum(val,sum,n); // Totally worth it ;)
+
+	}
+	return false;
+}
+//
+// 3. Count of all subsets: Gives count of all the subsetSums equal to given weight.
+//
+ll countSubsets(ll val[],ll W,ll n){
+	vvl dp(n+1, vector<ll> (W+1));
+	REP(i,n+1) REP(j,W+1){
+		if(i==0) dp[i][j] = 0;
+		if(j==0) dp[i][j] = 1;
+	}
+	REPN(i,n)
+	REPN(j,W){
+		if(val[i-1]<=j)
+			dp[i][j] = dp[i-1][j] + dp[i-1][j-val[i-1]];
+		else dp[i][j] = dp[i-1][j];
+	}
+
+		for(auto i: dp){ auto it = i; debug(it);}
+	return dp[n][W];
+}
+//
+// 4. Minimize Subset Sum Difference: Partition the array into two parts such that difference between the sums of each part become minimized. 
+//
+ll minimizeSubsetSumDifference(ll val[],ll n){
+	ll sum = accumulate(val, val + n, 0);
+	ll range = sum;
+	sum = ceil(sum/2.0);
+
+	vector<vector<bool>> dp(n+1, vector<bool> (sum+1));
+
+	REP(i,n+1)
+	REP(j,sum+1){
+		if(i==0) dp[i][j] = false;
+		if(j==0) dp[i][j] = true;
+	}
+
+	REPN(i,n) REPN(j,sum){
+		if(val[i-1] <= j)
+			dp[i][j] = dp[i-1][j-val[i-1]] or dp[i-1][j];
+		else dp[i][j] = dp[i-1][j];
+	}
+	for(auto i: dp){ auto it = i; debug(it);}
+
+	ll mi = 0;
+	for(ll j=0;j<sum;j++) if(dp[n][j]) mi = max(mi,j);
+
+	return (range - 2*mi);
+}
+//
+// 5. Count the number of Subset with a given difference: Find the number of such partitions in the list of elements such that the difference in the Subset Sums is equal to the given value.
+//
+ll countNumberSubsetDiff(ll val[],ll diff, ll n){
+	// using S1 - S2 = diff and S1 + S2 = sum
+	// we get that required sum = (diff + sum)/2 
+
+	ll sum = accumulate(val,val+n,0);
+	ll req_sum = (diff + sum) / 2;
+	return countSubsets(val,req_sum,n); // Refer the countSubset() function is required.
+}
+//
+// 6. Target Sum: From the given list of numbers, we need to return the count of different combinations by setting plus(+) or minus(-) signs infront of every element such that the sum becomes equal to the target.
+//
+ll targetSum(ll val[],ll W,ll n){
+	// consider the subsetsum of positive elements be S1 and that of negative elements be S2
+	// Now, S1 - S2 = W ( We may notice that it is similar to the previous problem. :D )
+	// Hence same implementation will be there.
+
+	return countNumberSubsetDiff(val,W,n); // Saving time ;)
 }
 
 void solve(){
@@ -119,17 +244,26 @@ void solve(){
 	
 	//0-1 Knapsack Input
 	ll wt[n] = {0}, vl[n] = {0};
-	REP(i,n){cin>>wt[i];} // weight array
 	REP(i,n){cin>>vl[i];} // value array
+	// REP(i,n){cin>>wt[i];} // weight array
 	ll W;
 	cin>>W; // capacity
 
 	// cout << zeroOneKnapsackRecursive(wt,vl,W,n);
 	// vvl dp(n+1, vector<ll> (W+1,-1)); // 2d vector declared with 'n+1' rows and 'W+1' columns with default value 1.
 	// cout<< zeroOneKnapsackMemoized(wt,vl,W,n,dp);
-	cout << zeroOneKnapsackTopDown(wt,vl,W,n);
+	// cout << zeroOneKnapsackTopDown(wt,vl,W,n);
+
+	// PROBLEMS ON 0-1 KNAPSACK
+	// cout<<( subsetSum(vl,W,n) ? "Yes" : "No");
+	// cout << (equalSumPartition(vl,n) ? "Yes" : "No");
+	// cout << countSubsets(vl,W,n);
+	// cout<< minimizeSubsetSumDifference(vl,n);
+	// cout << countNumberSubsetDiff(vl,W,n);
+	cout << targetSum(vl,W,n);
 
 }
+
 
 int main(){
 	#ifndef ONLINE_JUDGE
